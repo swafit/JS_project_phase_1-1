0d6f01e0f5ee8f35e667857ef74d1255ffdecc65
William <william.chalifoux@gmail.com>
2021-04-12 17:03:19 -0400

    Names

--- header.php
+++ header.php
@@ -9,6 +9,9 @@ include_once 'includes/functions.inc.php';
     <meta charset="utf-8">
     <title>Project</title>
     <!--I won't do more than barebone HTML, since this isn't an HTML tutorial.-->
+    <!--William Chalifoux-->
+    <!--Bradley Keith Steben -->
+    <!-- Duncan BH-->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
