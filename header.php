6f177497ea8476909054c8394629bac318d0eba1
BradleySteben <snadley10@gmail.com>
2021-04-16 14:42:01 -0400

    commit

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
   </head>
 
   session_start();
@@ -24,16 +25,6 @@
           <li><a href="index.html">Home</a></li>
           <li><a href="discover.html">About Us</a></li>
           <li><a href="community.html">Community</a></li>
-          <?php
-            if (isset($_SESSION["useruid"])) {
-              echo "<li><a href='profile.html'>Profile Page</a></li>";
-              echo "<li><a href='logout.html'>Logout</a></li>";
-            }
-            else {
-              echo "<li><a href='signup.html'>Sign up</a></li>";
-              echo "<li><a href='login.html'>Log in</a></li>";
-            }
-          ?>
         </ul>
       </div>
     </nav>
